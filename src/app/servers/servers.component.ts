0d31d1c85a5374332ba1f2d34d05486c7f6709e2
ironseiya <seiya.liulin@qq.com>
2018-10-02 21:44:54 +0800

    添加数据绑定、属性绑定、事件绑定

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,20 +2,37 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  serverCreationStatus = "No server was created!";
+  name: string = "first"
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+      this.name = "second";
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  getName() {
+    return this.name + " from method getName!";
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created!";
   }
 
 }
